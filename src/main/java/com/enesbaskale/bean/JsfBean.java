cd54809c4d01fe65e139ae7e8eb0c9edd6497154
Enes <enesbaskale@gmail.com>
2023-01-18 00:04:04 +0300

    Spring Framework App

---
+++ src/main/java/com/enesbaskale/bean/JsfBean.java
@@ -0,0 +1,20 @@
+package com.enesbaskale.bean;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+
+@Named(value = "cdiBean")
+@ApplicationScoped
+@Setter
+@Getter
+public class JsfBean {
+    private String patika;
+
+    public JsfBean() {
+        this.patika = "Springboot eğitimine hoş geldiniz!";
+    }
+
+
+}
