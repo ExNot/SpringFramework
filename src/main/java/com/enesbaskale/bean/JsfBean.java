b10665230ab062df6f2041a923cf08bdfcdb4784
Enes <enesbaskale@gmail.com>
2023-01-18 13:07:44 +0300

    @Named

--- src/main/java/com/enesbaskale/bean/JsfBean.java
+++ src/main/java/com/enesbaskale/bean/JsfBean.java
@@ -4,7 +4,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
-
+//
 @Named(value = "cdiBean")
 @ApplicationScoped
 @Setter
