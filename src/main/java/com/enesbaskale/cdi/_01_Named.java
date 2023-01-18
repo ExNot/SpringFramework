a23dec8a497b68cb1c63e2e40c67c7a9566fcce3
Enes <enesbaskale@gmail.com>
2023-01-18 13:04:12 +0300

    @Named

---
+++ src/main/java/com/enesbaskale/cdi/_01_Named.java
@@ -0,0 +1,24 @@
+package com.enesbaskale.cdi;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+
+//cdi ==> Bean
+@Named(value = "namedtuto")
+@ApplicationScoped
+public class _01_Named {
+    private Long namedId;
+    private String namedData = "Merhabalar Named Cdi Bean";
+
+}
---
+++ src/main/java/com/enesbaskale/tutorials/javaTutorials.java
@@ -0,0 +1,23 @@
+package com.enesbaskale.tutorials;
+
+public class javaTutorials {
+
+    /*
+    Super keyword
+    optional
+    cast
+    decoder encoder
+    instanceof
+    StringBuilder
+    StringBuffer
+    StringTokenizer
+    Enum yapıları
+    Multithread yapıları
+    S.o.l.i.d
+    interface
+    optional
+    lambda expression
+    method referances
+    design pattern
+    */
+}
